172e53c9720c27cd14acfce1a49f4ef9ec0bca96
Dave Abrahams <dabrahams@adobe.com>
2022-04-19 22:09:55 -0700

    Make it work

--- Tests.swift
+++ Tests.swift
@@ -2,33 +2,79 @@ import XCTest
 import LoftDataStructures_Zip2Collection
 import LoftTest_StandardLibraryProtocolChecks
 
+extension Zip2Collection: RandomAccessCollectionAdapter {
+    public typealias Base = Base0
+}
+
 final class Zip2CollectionTests: XCTestCase {
-  func testZipConformances() {
-    for n0 in 0...5 {
-      for n1 in 0...5 {
-        do {
-          let base0 = RandomAccessOperationCounter(0...n0)
-          let base1 = 0..<(n1 + 1)
-          let testSubject = Zip2Collection(base0: base0, base1: base1)
+  func test_0_0() {
+    checkZipConformance(length0: 0, length1: 0)
+  }
 
-          testSubject.checkRandomAccessCollectionLaws(
-            expecting: Swift.zip2(base0, base1),
-            operationCounts: base0.operationCounts
-          )
-        }
+  func test_0_1() {
+    checkZipConformance(length0: 0, length1: 1)
+  }
 
-        do {
-          let base0 = 0...n0
-          let base1 = RandomAccessOperationCounter(0..<(n1 + 1))
-          let testSubject = Zip2Collection(base0: base0, base1: base1)
+  func test_0_2() {
+    checkZipConformance(length0: 0, length1: 2)
+  }
 
-          testSubject.checkRandomAccessCollectionLaws(
-            expecting: Swift.zip2(base0, base1),
-            operationCounts: base1.operationCounts
-          )
-        }
-      }
+  func test_1_0() {
+    checkZipConformance(length0: 1, length1: 0)
+  }
+
+  func test_1_1() {
+    checkZipConformance(length0: 1, length1: 1)
+  }
+
+  func test_1_2() {
+    checkZipConformance(length0: 1, length1: 2)
+  }
+
+  func test_2_0() {
+    checkZipConformance(length0: 2, length1: 0)
+  }
+
+  func test_2_1() {
+    checkZipConformance(length0: 2, length1: 1)
+  }
+
+  func test_2_2() {
+    checkZipConformance(length0: 2, length1: 2)
+  }
+
+  func checkZipConformances(length0 n0: Int, length1 n1: Int) {
+    let expected = Swift.zip(0..<n0, 0..<n1).map(Pair.init)
+    do {
+      let base0 = RandomAccessOperationCounter(0..<n0)
+      let base1 = RandomAccessOperationCounter(0..<n1)
+      let testSubject = Zip2Collection(base0, base1)
+
+      testSubject.checkRandomAccessCollectionLaws(
+        expecting: expected,
+        operationCounts: base0.operationCounts
+      )
     }
+
+    do {
+      let base0 = RandomAccessOperationCounter(0..<n0)
+      let base1 = RandomAccessOperationCounter(0..<n1)
+      let testSubject = Zip2Collection(base0, base1)
+
+      testSubject.checkRandomAccessCollectionLaws(
+        expecting: expected,
+        operationCounts: base1.operationCounts
+      )
+    }
+
+    // Check heterogeneous case
+    zip(0..<n0, 0...n1)
+      .checkBidirectionalCollectionLaws(expecting: Swift.zip(0..<n0, 0...n1).map(Pair.init))
+
+    var subject = zip(Array(0..<n0), Array(0...n1))
+    subject.checkMutableCollectionLaws(
+      expecting: Swift.zip(0..<n0, 0...n1).map(Pair.init),
+      writing: Swift.zip(10..<n0 + 10, 20...(n1 + 20)).map(Pair.init))
   }
 }
 
--- Zip2Collection.swift
+++ Zip2Collection.swift
@@ -1,9 +1,52 @@
-public struct Zip2Collection<Base0: Collection, Base1: Collection>: Collection {
+public struct Pair<T0, T1> {
+  public var p0: T0
+  public var p1: T1
+
+  public init(_ p0: T0, _ p1: T1) {
+    (self.p0, self.p1) = (p0, p1)
+  }
+}
+
+extension Pair
+  : RandomAccessCollection,
+    BidirectionalCollection,
+    Collection, Sequence, MutableCollection
+  where T0 == T1
+{
+  public typealias Element = T0
+  public typealias Index = Int
+
+  public var startIndex: Int { 0 }
+  public var endIndex: Int { 2 }
+
+  public subscript(i: Int) -> T0 {
+    get { return i == 0 ? p0 : p1 }
+    set { if i == 0  { p0 = newValue } else { p1 = newValue } }
+  }
+}
+
+extension Pair: Equatable where T0: Equatable, T1: Equatable {}
+extension Pair: Hashable where T0: Hashable, T1: Hashable {}
+extension Pair: Comparable where T0: Comparable, T1: Comparable {
+  public static func <(l: Self, r: Self) -> Bool {
+    return l.p0 < r.p0 ? true
+      : l.p0 > r.p0 ? false
+      : l.p1 < r.p1
+  }
+}
+
+private protocol Zip2RandomAccessCollection {
+  func _normalize(_ ip: UnsafeMutableRawPointer)
+}
+
+public struct Zip2Collection<Base0: Collection, Base1: Collection> : Collection
+{
   public var base0: Base0
   public var base1: Base1
 
-  public func makeIterator() -> Zip2Sequence<Base0, Base1>.Iterator {
-    zip(base0, base1).makeIterator()
+  public init(_ base0: Base0, _ base1: Base1) {
+    self.base0 = base0
+    self.base1 = base1
   }
 
   public struct Index: Comparable {
@@ -19,10 +62,10 @@ public struct Zip2Collection<Base0: Collection, Base1: Collection>: Collection {
     }
   }
 
-  public typealias Element = (Base0.Element, Base1.Element)
+  public typealias Element = Pair<Base0.Element, Base1.Element>
 
   public subscript(i: Index) -> Element {
-    get { (base0[i.base0], base1[i.base1]) }
+    get { .init(base0[i.base0], base1[i.base1]) }
   }
 
   public func index(after i: Index) -> Index {
@@ -34,58 +77,130 @@ public struct Zip2Collection<Base0: Collection, Base1: Collection>: Collection {
     base1.formIndex(after: &i.base1)
   }
 
-  public var startIndex: Index { Index(base0: base0.startIndex, base1: base1.startIndex)  }
-  public var endIndex: Index { Index(base0: base0.endIndex, base1: base1.endIndex)  }
-}
-
-extension Zip2Collection: MutableCollection where Base0: MutableCollection, Base1: MutableCollection
-{
-  public subscript(i: Index) -> Element {
-    get { (base0[i.base0], base1[i.base1]) }
-    set { base0[i.base0] = newValue.0; base1[i.base1] = newValue.1 }
+  public var startIndex: Index {
+    Index(base0: base0.startIndex, base1: base1.startIndex)
   }
-}
 
-extension Zip2Collection: RandomAccessCollection, BidirectionalCollection
-  where Base0: RandomAccessCollection, Base1: RandomAccessCollection
-{
-  public var count: Int { Swift.min(base0.count, base1.count) }
+  public var endIndex: Index {
+    Index(base0: base0.endIndex, base1: base1.endIndex)
+  }
 
-  public func formIndex(i: inout Index, offsetBy n: Int) {
-    defer {
-      base0.formIndex(&i.base0, offsetBy: n)
-      base1.formIndex(&i.base1, offsetBy: n)
+  public func distance(from i: Index, to j: Index) -> Int {
+    if i == j {
+      return 0
     }
-
-    if n >= 0 {
-      _onFastPath();
-      return
+    else if i < j {
+      return Swift.min(
+        base0.distance(from: i.base0, to: j.base0),
+        base1.distance(from: i.base1, to: j.base1))
     }
+    else {
+      return -Swift.min(
+        base0.distance(from: j.base0, to: i.base0),
+        base1.distance(from: j.base1, to: i.base1))
 
-    let lengthDifference = base1.count - base0.count
-    if lengthDifference < 0 {
-      if i.base1 == base1.endIndex {
-        base1.formIndex(&i.base1, offsetBy: lengthDifference)
-      }
-      else if i.base0 == base0.endIndex {
-        base0.formIndex(&i.base0, offsetBy: -lengthDifference)
-      }
     }
   }
 
-  public func index(_ i: Index, offsetBy n: Int) -> Index {
+  public func formIndex(_ i: inout Index, offsetBy n: Int) {
+    if n < 0, i == endIndex, let me = self as? Zip2RandomAccessCollection {
+      me._normalize(&i)
+    }
+    else { _onFastPath() }
+
+    base0.formIndex(&i.base0, offsetBy: n)
+    base1.formIndex(&i.base1, offsetBy: n)
+  }
+
+  public func formIndex(_ i: inout Index, offsetBy n: Int, limitedBy l: Index) -> Bool {
+    var l1 = l
+    if i == endIndex, let me = self as? Zip2RandomAccessCollection {
+      me._normalize(&i)
+      me._normalize(&l1)
+    }
+    else { _onFastPath() }
+
+    let r0 = base0.formIndex(&i.base0, offsetBy: n, limitedBy: l1.base0)
+    let r1 = base1.formIndex(&i.base1, offsetBy: n, limitedBy: l1.base1)
+    assert(r0 == r1)
+    return r1
+  }
+
+  public func index(_ i: Index, offsetBy n:Int) -> Index {
     var r = i
     formIndex(&r, offsetBy: n)
     return r
   }
 
+  public func index(_ i: Index, offsetBy n:Int, limitedBy l: Index) -> Index? {
+    var p = i
+    let success = formIndex(&p, offsetBy: n, limitedBy: l)
+    return success ? p : nil
+  }
+
+  public var count: Int { Swift.min(base0.count, base1.count) }
+}
+
+extension Zip2Collection: MutableCollection where Base0: MutableCollection, Base1: MutableCollection
+{
+  public subscript(i: Index) -> Element {
+    get { .init(base0[i.base0], base1[i.base1]) }
+    set { base0[i.base0] = newValue.p0; base1[i.base1] = newValue.p1 }
+  }
+}
+
+
+extension Zip2Collection
+  : RandomAccessCollection, BidirectionalCollection, Zip2RandomAccessCollection
+  where Base0: RandomAccessCollection, Base1: RandomAccessCollection
+{
   public func index(before i: Index) -> Index {
     var r = i
-    formIndex(before: &r)
+    _formIndex(&r, negativeOffsetBy: -1)
     return r
   }
 
-  public func formIndex(before i: inout Index) {
-    formIndex(&i, offsetBy: -1)
+  fileprivate func _normalize(_ ip_: UnsafeMutableRawPointer) {
+    let ip = ip_.assumingMemoryBound(to: Index.self)
+    let lengthDifference = base1.count - base0.count
+    if lengthDifference > 0 {
+      if ip.pointee.base1 == base1.endIndex {
+        base1.formIndex(&ip.pointee.base1, offsetBy: -lengthDifference)
+      }
+    }
+    else {
+      if ip.pointee.base0 == base0.endIndex {
+        base0.formIndex(&ip.pointee.base0, offsetBy: lengthDifference)
+      }
+    }
+  }
+
+  fileprivate func _formIndex(_ ip_: UnsafeMutableRawPointer, negativeOffsetBy n:Int) {
+    assert(n < 0)
+    let ip = ip_.assumingMemoryBound(to: Index.self)
+    _normalize(&ip.pointee)
+    base0.formIndex(&ip.pointee.base0, offsetBy: n)
+    base1.formIndex(&ip.pointee.base1, offsetBy: n)
+  }
+
+  fileprivate func _formIndex(
+    _ ip_: UnsafeMutableRawPointer, negativeOffsetBy n:Int, limitedBy lp_: UnsafeRawPointer) -> Bool
+  {
+    assert(n < 0)
+    let ip = ip_.assumingMemoryBound(to: Index.self)
+    let lp = lp_.assumingMemoryBound(to: Index.self)
+    _normalize(&ip.pointee)
+    let r0 = base0.formIndex(&ip.pointee.base0, offsetBy: n, limitedBy: lp.pointee.base0)
+    let r1 = base1.formIndex(&ip.pointee.base1, offsetBy: n, limitedBy: lp.pointee.base1)
+    assert(r0 == r1)
+    return r1
   }
 }
+
+public func zip<C0: Collection, C1: Collection>(_ c0: C0, _ c1: C1) -> Zip2Collection<C0, C1> {
+  Zip2Collection(c0, c1)
+}
+
+// Local Variables:
+// fill-column: 100
+// End:
